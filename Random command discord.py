c5ae1b0c358aeb2223734c33ca6c28e98aff524d
brother <hacker@gmail.com>
2025-02-28 19:18:22 +0300

    update

--- Random command discord.py
+++ Random command discord.py
@@ -9,13 +9,13 @@ import time
 
 bot = commands.Bot(command_prefix="*", intents=disnake.Intents.all())
 
-@bot.slash_command(description="Команда выдаст рандомное число между выбраными цифрами") #us Translator
+@bot.slash_command(description="Команда выдаст рандомное число между выбраными цифрами") #use Translator
 async def random_number(ctx, num1: int, num2: int):
     if num1 >= num2:
-        await ctx.send("Первое число должно быть меньше второго числа.") #us Translator
+        await ctx.send("Первое число должно быть меньше второго числа.") #use Translator
         return
 
     result = random.randint(num1, num2)
     await ctx.send(f"Случайное число между {num1} и {num2}: {result}")
 
- bot.run(" ") #bot id
+bot.run(" ") #bot id~
